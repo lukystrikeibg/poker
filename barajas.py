d3db6794ac32d5944ccfbb7f0776a458b18976de
¨lukas¨ <lukystrikeibg@gmail.com>
2020-07-15 13:39:18 -0500

    ¨funciones¨

--- barajas.py
+++ barajas.py
@@ -4,7 +4,38 @@ import collections
 PALOS =['espada ', 'corazon', 'rombo' , 'trebol']
 VALORES=['as','2','3','4','5','6','7 ','8', '9',' 10', 'j', 'q', ' k']
 
+def crear_baraja():
+    barajas=[]
+    for palo in PALOS:
+      for valor in VALORES:
+            barajas.append ((palo , valor))
 
+    return barajas
+
+
+
+def main (tamano_mano, intento):
+    barajas = crear_baraja()
+
+    manos = []
+
+    for _ in range (intentos):
+        mano = obtener_mano(barajas,tamano_mano)
+        manos.append(mano)
+
+    pares = 0
+
+    for mano in manos:
+        valores=[]
+        for carta in mano:
+            valores.append(carta[1])
+
+
+        counter= dict(collections.Counter(valores))
+        for val in counter.values():
+            if val==3:
+                pares +=1
+                break
 
 
 
